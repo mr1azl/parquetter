bcf5e1e16ab574684ad5650faf880a002d014f8b
Your Name <you@example.com>
2015-08-22 12:48:47 +0000

    Spark Standalone Cluster

--- src/main/scala/sparky/com/samklr/adm/Transformer.scala
+++ src/main/scala/sparky/com/samklr/adm/Transformer.scala
@@ -14,10 +14,10 @@ object Transformer extends App{
 
   val conf = new SparkConf()
     .setAppName("Parquetter")
-    .setMaster("local[*]")
+    .setMaster("spark://spark:7077")
     .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     .set("spark.kryo.registrator", classOf[MyKryoRegistrator].getName)
-    .set("spark.executor.memory","3G")
+    .set("spark.executor.memory","10G")
 
   val sparkContext = new SparkContext(conf)
 
@@ -31,30 +31,29 @@ object Transformer extends App{
   println(sparkContext.getConf.toDebugString)
 
 
- val flat_logs = sparkContext.textFile("/media/samklr/windows/code/latticeiq/log_adm_1M.json",32)
- //val flat_logs = sparkContext.textFile("/home/samklr/data/*")
-     .mapPartitions(line => Logs.parseFromJson(line))
-                              .map(r => if ((r != null) && (r.coordinates != null))
+ val flat_logs = sparkContext.textFile("/home/samklr/data/*",100)
+                             .mapPartitions(line => Logs.parseFromJson(line))
+                             .map(r => if ((r != null) && (r.coordinates != null))
                                           FlatLog( r.coordinates(1), r.coordinates(0), r.impflag, r.log_date,
                                                 r.log_day, r.log_hour, r.log_tkn, r.pubuid, r.userid)
                                           else FlatLog (0.0, 0.0, 0, "NA", "NA", 0, "NA","NA", "NA"))
-                              .filter (log => (log.userid != "NA") || (log.lat != 0.0) || (log.lon != 0) || (log.pubuid != "NA"))
-                              .persist(StorageLevel. MEMORY_ONLY_SER)
+                             .filter (log => (log.userid != "NA") || (log.lat != 0.0) || (log.lon != 0) || (log.pubuid != "NA"))
+                             .persist(StorageLevel.MEMORY_AND_DISK_SER)
 
   val flatDF=sqlContext.createDataFrame[FlatLog](flat_logs).na.drop
 
-  flatDF.printSchema()
+ // flatDF.printSchema()
 
-  flatDF.show(10)
+  //flatDF.show(10)
 
-//  val sz= flatDF.count
+  val sz= flatDF.count
 
-//  println("DataFrame size " + sz)
+  println("DataFrame size " + sz)
 
   flatDF.write
         .format("parquet")
         .partitionBy("log_day")
-        .save("/media/samklr/windows/code/latticeiq/log_adm.1M.snappy.parquet")
+        .save("/home/samklr/data.adm.parquet")
 
   //flatDF.write.parquet("/home/samklr/data.parq.lz")
 
